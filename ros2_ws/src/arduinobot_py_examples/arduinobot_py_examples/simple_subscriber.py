ac63e684e2a72f192d43142b3a23f998038df21a
Ambooken-LinuxDev-004 <joeambooken04@gmail.com>
2025-11-06 11:17:54 +0530

    Learned subscriber code in python

---
+++ ros2_ws/src/arduinobot_py_examples/arduinobot_py_examples/simple_subscriber.py
@@ -0,0 +1,29 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+
+class SimpleSubscriber(Node):
+    def __init__(self):
+        super().__init__("simple_subscriber")
+        self.sub = self.create_subscription(String, "chatter", self.msgCallBack, 10)
+
+    def msgCallBack(self, msg):
+        self.get_logger().info("I heard %s" % msg.data)
+
+
+
+
+def main():
+    rclpy.init()
+    simple_subscriber_obj = SimpleSubscriber()
+    rclpy.spin(simple_subscriber_obj)
+    simple_subscriber_obj.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
